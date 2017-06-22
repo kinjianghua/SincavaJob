c3f4845999bdc57ff3dbc3aa6fe2fc91bdfab6f3
kinjianghua <kinjianghua@gmail.com>
2017-06-22 15:53:44 +0900

    update index.php

--- index.php
+++ index.php
@@ -14,49 +14,6 @@
 	// var_dump($b);     // string(7) "foo bar
 	// var_dump($c);     // array(3) { [0]=> int(1) [1]=> int(2) [2]=> int(3) }
 
-
-
-	// error_reporting(E_ALL | E_STRICT);
-	// ini_set('display_errors', 'On');
-
-    // $var = 1;
-    // $var_name = 'var';
-    // echo $$var_name, PHP_EOL;
-
-    // $array = array(1, 2, 3, 4, 7);
-
-    // foreach ($array as $i) {
-    // 	echo $i, PHP_EOL;   // 1 ...
-
-
-    // }
-
-    // echo "Last:", $i, PHP_EOL;
-
-	// $foo = 1;      // グローバル
-	// $bar = 3;
- //    $mama = 100; 
-
-
-	// function some_function() {
-	// 	$foo = 10; // ローカル
-	// 	$bar = 20; //
-	// 	global $mama; 
-	// 	echo "内部 : ", $foo, PHP_EOL;
-	// 	echo "内部 : ", $bar, PHP_EOL;
-	// 	echo "内部 : ", $mama, PHP_EOL;
-
-	// }
-
-	// echo $foo, PHP_EOL;
-	// echo $bar, PHP_EOL;
-	// echo $mama, PHP_EOL;
-
-	// // echo ">>> >>> ", $argc;
-
-	// some_function();
-
-
 	$kk = new MyClass();
 	$kk->work();
     $kk->eat();
