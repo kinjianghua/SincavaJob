b8f946b7b59ed085f6ab6614dae93c44cc30eacb
kinjianghua <kinjianghua@gmail.com>
2017-06-23 12:32:42 +0900

    add override

--- lesson0623.php
+++ lesson0623.php
@@ -3,7 +3,10 @@
 
 	$student01 = new Student("xiaoming",80,"A");
 	$student01->test();
+	$student01->eat();
 
+	$sthdent02 = new Excellent_Sthdent("aaa",40,"C");
+    $sthdent02->eat("sha");
 
 	class Student{
 
@@ -15,6 +18,10 @@
 			echo  $this->name, "学生が受験している", "成績は",$this->score, "レベル",$this->level;
 		}
 
+		public function eat($something = "ライス") {
+			echo $something, "食べられる";
+		}
+
 
 		public function __construct($name, $aaa, $bbb){
 			$this->name = $name;
@@ -23,11 +30,15 @@
 		}
 
 		public function __destruct(){
-
 			//echo "__destruct";
 		}
 	}
 
+	class Excellent_Sthdent extends Student{
+		public function eat($anything){
+			echo  $anything, "食べられる";
+		}
+	}
 
 
 
